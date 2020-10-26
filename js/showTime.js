bf0bd7a82e6c5aaaba7928a058d729fd9b4405b3
dht66 <8163329+dht66@user.noreply.gitee.com>
2020-10-27 01:35:08 +0800

    第一次提交

---
+++ js/showTime.js
@@ -0,0 +1,19 @@
+			var t = null;
+			time();
+			t = setTimeout(time, 1000); //開始运行
+			function time() {
+				clearTimeout(t);//清除定时器
+				dt = new Date();
+				var y = dt.getFullYear();
+				var mt = dt.getMonth() + 1;
+				var day = dt.getDate();
+				var h = dt.getHours(); //获取时
+				h = h < 10 ? "0" + h : h;
+				var m = dt.getMinutes(); //获取分
+				m = m < 10 ? "0" + m : m;
+				var s = dt.getSeconds(); //获取秒
+				s = s< 10 ? "0" + s : s;
+				document.querySelector(".showTime").innerHTML = '当前时间：' + y + "年" + mt + "月" + day + '日' + "-" + h + "时" + m + "分" +
+					s + "秒";
+				t = setTimeout(time, 1000); //设定定时器，循环运行     
+			}
